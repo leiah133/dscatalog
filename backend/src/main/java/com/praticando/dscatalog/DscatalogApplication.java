8ecbe54a89d86ff1fd76a591eda296df80db06b7
Léia Oliver <106698637+leiah133@users.noreply.github.com>
2023-07-19 21:55:25 -0300

    Backend project created

---
+++ backend/src/main/java/com/praticando/dscatalog/DscatalogApplication.java
@@ -0,0 +1,16 @@
+package com.praticando.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class DscatalogApplication {
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DscatalogApplication.class, args);
+	}
+}
